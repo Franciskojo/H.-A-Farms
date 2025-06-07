07a23e3066cef6896f349d5798786289590477e3
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2025-06-07 12:38:07 +0000

    added a file

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -1,3 +1,18 @@
+//    const hamburgerBtn = document.getElementById('hamburgerBtn');
+//     const sidebar = document.querySelector('.admin-sidebar');
+
+//     hamburgerBtn.addEventListener('click', () => {
+//         sidebar.classList.toggle('show');
+//     });
+
+//     // Optional: Hide sidebar when clicking outside
+//     document.addEventListener('click', function (e) {
+//         if (!sidebar.contains(e.target) && !hamburgerBtn.contains(e.target)) {
+//             sidebar.classList.remove('show');
+//         }
+//     });
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // Initialize charts
     const salesCtx = document.getElementById('salesChart').getContext('2d');
