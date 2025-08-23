7532280480edf8a945e1d9b26d21c68f6d48adf7
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2025-08-23 14:11:37 +0000

    changes

--- js/admin-users.js
+++ js/admin-users.js
@@ -63,7 +63,10 @@ function renderUsers(users) {
         </td>
         <td>${new Date(user.createdAt).toLocaleDateString()}</td>
         <td>
-
+          <div class="table-actions">
+            <button class="btn-edit" data-userid="${user.id}">Edit</button>
+            <button class="btn-delete" data-userid="${user.id}">Delete</button>
+          </div>
         </td>
       </tr>
     `;
